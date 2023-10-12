50bf2a8cd4ef02f53ca1892f66680929572c8df9
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-10-12 19:25:01 +0530

    Downloaded Product Images

--- src/store/useCartStore.js
+++ src/store/useCartStore.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', {
+export const useCartStore = defineStore('cart', {
     // state: () => {
     //     return {
     //         count: 0,
@@ -8,15 +8,36 @@ export const useCounterStore = defineStore('counter', {
     //     }
     // },
     state: () => ({
-        count: 0,
-        name: 'Aniket',
+        // https://biteable.com/tools/image-resizer/#instagram
+        products: [
+            {
+                id: 1,
+                name: 'Iphone 12',
+                price: 700,
+                image: '/public/images/Iphone.jpg',
+            },
+            {
+                id: 2,
+                name: 'Samsung s10',
+                price: 400,
+                image: '/public/images/Samsung.jpg',
+            },
+            {
+                id: 3,
+                name: 'Samsung Tv',
+                price: 1200,
+                image: '/public/images/tv.jpg',
+            },
+            {
+                id: 4,
+                name: 'Huwawei Mate',
+                price: 900,
+                image: '/public/images/huwawei.jpg',
+            },
+        ],
     }),
     getters: {
-        doubleCount: (state) => state.count * 2,
+        getProducts: (state) => state.products,
     },
-    actions: {
-        increment() {
-            this.count++
-        },
-    },
+    actions: {},
 })
